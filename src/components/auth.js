1a34de1dfce52de29d0c2872557a38dcd8e52b92
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2021-09-22 22:02:38 +0100

    setup token login and localstorage

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// styles
+import "../styles/auth.css";
+
+// login endpoint
+const loginUrl = "http://localhost:8000/gateway/login";
 
 export default function _Auth(){
+
+    let _storage = window.localStorage;
+    
+    useEffect(()=>{
+        if (!(_storage.getItem("token"))){
+            _storage.setItem("token", {});
+        }
+    }, []);
+
+    const _login = (url, payload) =>{
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(payload),
+
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp=>{
+            if (!resp.ok){
+                throw new Error("Response was not Ok");
+            }else{
+                // console.log(resp.json());
+                return resp.json();
+            }
+        })
+        .then(resp=>{
+            console.log(resp);
+            setToken(resp);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+    const [loginCredentials, setLoginCredentials] = useState({username: "", password: ""});
+    const [error, setError] = useState({type: "", message: ""});
+    const [token, setToken] = useState();
+
+    const handleChange = (e) =>{
+        const fieldName = e.target.name;
+        const value = e.target.value;
+
+        // set credential details
+        setLoginCredentials({...loginCredentials, [fieldName]: `${value}`});
+    };
+
+    // set token in local storage
+
+    {token && _storage.setItem("token", JSON.stringify({access_token: token.access_token, refresh_token: token.refresh_token}))}
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if (!(loginCredentials.username && loginCredentials.password)){
+            setError({type: "Field Error", message: "Fill in all fields"});
+        }else{
+            setError({type: "", message: ""});
+            console.log(loginCredentials);
+            _login(loginUrl, {
+                username: loginCredentials.username,
+                password: loginCredentials.password
+            })
+        }
+    }
+
     return (
-        <h2>Login</h2>
+        <React.Fragment>
+            <div className="overlay"></div>
+            <div className="login">
+                <div className={error.type ? "error" : ""}>
+                    {error && <h2>{error.message}</h2>}
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input onChange={handleChange} type="text" id="username" name="username" />
+                    <label htmlFor="password">Password</label>
+                    <input onChange={handleChange} type="password" id="password" name="password" />
+                    <button className="login-btn">login</button>
+                </form>
+            </div>
+        </React.Fragment>
     )
 }
